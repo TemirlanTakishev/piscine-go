c3fdd6845326c6ee8873e5d6e1a6e3b65ca0e0fe
TemirlanTakishev <temirlan.takishev@mail.ru>
2019-10-16 17:04:12 +0600

    wdat

--- printcomb.go
+++ printcomb.go
@@ -1,20 +1,23 @@
-package piscine
+package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func PrintComb() {
-
-	for a := '0'; a <= '7'; a++ {
-		for b := '1'; b <= '9'; b++ {
-			for c := '2'; c <= '9'; c++ {
+	for a := '0'; a <= '9'; a++ {
+		for b := a + 1; b <= '9'; b++ {
+			for c := b + 1; c <= '9'; c++ {
 				z01.PrintRune(a)
 				z01.PrintRune(b)
 				z01.PrintRune(c)
-				z01.PrintRune(44)
-				z01.PrintRune(32)
+				if a < 55 {
+					z01.PrintRune(44)
+					z01.PrintRune(32)
+				}
 			}
 		}
 	}
+}
+
+func main() {
+	PrintComb()
 }
