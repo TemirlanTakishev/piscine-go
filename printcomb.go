2bc5c53869bb07de8f5e8d9d7072bcb71cc7589b
TemirlanTakishev <temirlan.takishev@mail.ru>
2019-10-16 16:45:04 +0600

    dawo

--- printcomb.go
+++ printcomb.go
@@ -6,12 +6,12 @@ import (
 
 func PrintComb() {
 
-	for t := '0'; a <= '7'; a++ {
-		for e := '1'; b <= '9'; b++ {
-			for m := '2'; c <= '9'; c++ {
-				z01.PrintRune(t)
-				z01.PrintRune(e)
-				z01.PrintRune(m)
+	for a := '0'; a <= '7'; a++ {
+		for b := '1'; b <= '9'; b++ {
+			for c := '2'; c <= '9'; c++ {
+				z01.PrintRune(a)
+				z01.PrintRune(b)
+				z01.PrintRune(c)
 				z01.PrintRune(44)
 				z01.PrintRune(32)
 			}
