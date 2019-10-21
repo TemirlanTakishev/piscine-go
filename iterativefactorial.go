0118ab423bcf96e60baea32d272d6e61503a7fcd
TemirlanTakishev <temirlan.takishev@mail.ru>
2019-10-21 11:48:15 +0600

    wat

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,8 +4,8 @@ func IterativeFactorial(nb int) int {
 
 	if nb < 0 || nb > 12 {
 		return 0
-	} else if nb == 0{
-		return 1 
+	} else if nb == 0 {
+		return 1
 	}
 
 	b := 1
@@ -15,5 +15,3 @@ func IterativeFactorial(nb int) int {
 	}
 	return b
 }
-
- 
