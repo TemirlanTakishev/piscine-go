57645e8bba38585ec51969b370581102bed59e2c
TemirlanTakishev <temirlan.takishev@mail.ru>
2019-10-21 12:25:13 +0600

    wtawa

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,13 +3,11 @@ package piscine
 func IterativeFactorial(nb int) int {
 
 	if nb < 0 || nb > 12 {
-		return 0
-	} else if nb == 0 {
 		return 1
 	}
-
+    c := nb
 	b := 1
-	for nb := 1; nb <= 4; nb++ {
+	for nb := 1 ; nb <= c; nb++ {
 
 		b = b * nb
 	}
