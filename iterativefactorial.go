debf557c7f20bef643fb7777a79ee3807e62a9d9
TemirlanTakishev <temirlan.takishev@mail.ru>
2019-10-21 11:30:42 +0600

    wdat

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,11 +1,21 @@
-package piscine
+package main
+
+import "fmt"
 
 func IterativeFactorial(nb int) int {
-
+ 
+	if nb != 4 {
+	 return nb
+	} 
 	b := 1
 	for nb := 1; nb <= 4; nb++ {
 
-		b = b * nb
+		b = b * nb 
 	}
 	return b
+}
+
+func main() {
+	arg := 4
+	fmt.Println(IterativeFactorial(arg))
 }
