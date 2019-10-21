65511cda8f34009414ff8a89cdb915cd2c39ae60
TemirlanTakishev <temirlan.takishev@mail.ru>
2019-10-22 00:46:40 +0600

    dwar

--- iterativepower.go
+++ iterativepower.go
@@ -1,11 +1,12 @@
 package piscine
 
 func IterativePower(nb int, power int) int {
-	if nb < 0 || nb > 12 {
+	if power < 0 {
 		return 0
-	} else if nb == 0 {
+	} else if power == 0 {
 		return 1
 	}
+
 	b := 1
 	for i := 1; i <= nb; i++ {
 
