416a2195dda1031f419b710d6c6d99027edad932
TemirlanTakishev <temirlan.takishev@mail.ru>
2019-10-21 15:14:54 +0600

    wdat

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -2,11 +2,11 @@ package piscine
 
 func RecursiveFactorial(nb int) int {
 
-	if nb == 1{
-		return 1 
+	if nb == 1 {
+		return 1
 	}
-if nb > 1 {
-	return nb * RecursiveFactorial( nb -1 )
-}
-    return 0 
+	if nb > 1 {
+		return nb * RecursiveFactorial(nb-1)
+	}
+	return 0
 }
