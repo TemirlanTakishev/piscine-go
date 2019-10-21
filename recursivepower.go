2e96f90a53d0378dd88456a207bd1969a4c468b5
TemirlanTakishev <temirlan.takishev@mail.ru>
2019-10-22 04:20:34 +0600

    dwat

--- recursivepower.go
+++ recursivepower.go
@@ -1,10 +1,11 @@
 package piscine
 
 func RecursivePower(nb int, power int) int {
-
 	if power == 0 {
 		return 1
-	} else if nb < 1 {
+	}
+
+	if power < 0 {
 		return 0
 	}
 	return nb * RecursivePower(nb, power-1)
