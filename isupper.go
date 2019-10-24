5e132a9fd2c9107c4274a87e75a1dbf208ed7f09
TemirlanTakishev <temirlan.takishev@mail.ru>
2019-10-24 16:20:36 +0600

    dw

---
+++ isupper.go
@@ -0,0 +1,20 @@
+package piscine
+
+func IsUpper(str string) bool {
+
+	x := []rune(str)
+	len := 0
+
+	for range str {
+		len++
+	}
+	for i := 0; i < len; i++ {
+
+		if x[i] >= 'A' && x[i] <= 'Z' {
+		} else {
+			return true
+		}
+	}
+
+	return false
+}
