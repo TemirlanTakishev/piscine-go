c00cd90bd2863c09b25847d783342d25c9928ee9
TemirlanTakishev <temirlan.takishev@mail.ru>
2019-10-24 15:02:24 +0600

    wdat

--- test/test.go
+++ test/test.go
@@ -7,9 +7,6 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.IsAlpha("Hello! How are you?"))
-	fmt.Println(piscine.IsAlpha("HelloHowareyou"))
-	fmt.Println(piscine.IsAlpha("What's this 4?"))
-	fmt.Println(piscine.IsAlpha("Whatsthis4"))
-	fmt.Println(piscine.IsAlpha("dwada  "))
+	fmt.Println(piscine.IsNumeric("010203"))
+	fmt.Println(piscine.IsNumeric("01,02,03"))
 }
