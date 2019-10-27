ebde96129eebc8671686a62dd9663517c6b3a896
TemirlanTakishev <temirlan.takishev@mail.ru>
2019-10-28 03:49:43 +0600

    dwaad

---
+++ concatparams.go
@@ -0,0 +1,18 @@
+package piscine
+
+func ConcatParams(args []string) string {
+	result := ""
+	k := 0
+
+	for index := range args {
+		k = index
+	}
+
+	for index, value := range args {
+		result += value
+		if index != k {
+			result += "\n"
+		}
+	}
+
+	return result
